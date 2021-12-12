77d4c1a898864176d6a339fd2bd9e56b7119af69
Dimitrios Dimitriadis <melkorinos@gmail.com>
2021-12-12 08:51:17 +0100

    File name update

--- src/test/java/gerimedica/Tests/TestSteps.java
+++ src/test/java/gerimedica/Tests/TestSteps.java
@@ -13,7 +13,6 @@ import java.time.Duration;
 import java.util.List;
 
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -24,7 +23,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import gerimedica.Pages.CollectionPage;
 import gerimedica.Pages.LandingPage;
 
-public class Tests {
+public class TestSteps {
 
     LandingPage landingPage;
     CollectionPage collection;
